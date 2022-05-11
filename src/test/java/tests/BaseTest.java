5fe7271394c4f3d9ba09e3cf4005d55614c045ca
Bouqui <bukkybridget@gmail.com>
2022-05-11 10:04:42 +0100

    Removed unused imports

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,16 +1,10 @@
 package tests;
 
 import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
-
 import java.util.HashMap;
-import java.util.Map;
 
 public class BaseTest{
-    String email;
-    String password;
     String token = "dcd98ae3aae0555fea80382665329712";
 
     @BeforeClass(alwaysRun = true)
